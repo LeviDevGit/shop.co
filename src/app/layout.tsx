d3438fdcb5ba578a014da5fb7445b2bf53a5b133
Levi <114665569+LeviDevGit@users.noreply.github.com>
2024-08-20 21:00:14 -0300

    :iphone: feat: desktop 1024px responsividade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,17 +78,33 @@ export default function RootLayout({
               </button>
             </div>
           </div>
-          <nav className="flex items-end justify-between p-3 pb-5">
-            <div className="flex items-end gap-3">
-              <button>
+          <nav className="mx-4 flex items-end justify-between py-3 pb-5 lg:m-0 lg:items-center lg:px-24">
+            <div className="flex items-end gap-4">
+              <button className="lg:hidden">
                 <Menu strokeWidth={2.5} />
               </button>
               <h2 className={`${integral.className} text-3xl font-bold`}>
                 SHOP.CO
               </h2>
             </div>
-            <div className="flex gap-3">
-              <button>
+            <ul className="hidden lg:flex">
+              <li>Shop</li>
+              <li>On Sale</li>
+              <li>New Arrivals</li>
+              <li>Brands</li>
+            </ul>
+            <div className="hidden lg:block">
+              <div className="flex h-[42px] items-center gap-3 rounded-[62px] bg-anti-flash-white p-5 text-black/40 lg:h-[50px]">
+                <Search />
+                <input
+                  type="text"
+                  placeholder="Enter your email address"
+                  className="w-full bg-transparent outline-none"
+                />
+              </div>
+            </div>
+            <div className="flex gap-3 lg:gap-4">
+              <button className="lg:hidden">
                 <Search strokeWidth={2.5} />
               </button>
               <button>
@@ -103,14 +119,14 @@ export default function RootLayout({
         <main>{children}</main>
         <footer>
           <div className="custom-gradient lg:px-24">
-            <div className="mx-4 justify-between gap-2 rounded-[20px] bg-black p-7 lg:m-0 lg:flex">
+            <div className="mx-4 rounded-[20px] bg-black p-7 lg:m-0 lg:flex lg:items-center lg:justify-between lg:p-10 lg:px-20">
               <p
-                className={`${integral.className} mb-9 text-left text-3xl font-black text-white`}
+                className={`${integral.className} mb-9 text-left text-3xl font-black text-white lg:mb-0 lg:w-5/12 lg:text-5xl`}
               >
                 STAY UPTO DATE ABOUT OUR LATEST OFFERS
               </p>
-              <div className="flex flex-col items-stretch gap-4 text-black/40">
-                <div className="flex h-[42px] items-center gap-3 rounded-[62px] bg-white p-5">
+              <div className="flex flex-col items-stretch gap-4 text-black/40 lg:w-1/4 lg:text-lg">
+                <div className="flex h-[42px] items-center gap-3 rounded-[62px] bg-white p-5 lg:h-[50px]">
                   <Mail />
                   <input
                     type="text"
@@ -118,102 +134,118 @@ export default function RootLayout({
                     className="w-full outline-none"
                   />
                 </div>
-                <button className="h-[42px] rounded-[62px] bg-white font-medium text-black">
+                <button className="h-[42px] rounded-[62px] bg-white font-medium text-black lg:h-[50px]">
                   Subscribe to Newsletter
                 </button>
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-3 bg-anti-flash-white px-4 text-black lg:px-24">
-            <h2 className={`${integral.className} text-3xl text-black`}>
-              SHOP.CO
-            </h2>
-            <p className="text-black/60">
-              We have clothes that suits your style and which you’re proud to
-              wear. From women to men.
-            </p>
-            <ul className="flex gap-3">
-              <li className="rounded-full border border-black/20 bg-white p-2">
-                <a href="">
-                  <Twitter />
-                </a>
-              </li>
-              <li className="rounded-full border border-black/20 bg-white p-2">
-                <a href="">
-                  <Facebook />
-                </a>
-              </li>
-              <li className="rounded-full border border-black/20 bg-white p-2">
-                <a href="">
-                  <Instagram />
-                </a>
-              </li>
-              <li className="rounded-full border border-black/20 bg-white p-2">
-                <a href="">
-                  <Github />
-                </a>
-              </li>
-            </ul>
-            <ul className="mb-3 grid grid-cols-2 gap-y-6">
-              <li>
-                <h3 className="mb-3 font-medium text-black">COMPANY</h3>
-                <div className="flex flex-col gap-2 text-black/60">
-                  <a href="">About</a>
-                  <a href="">Features</a>
-                  <a href="">Works</a>
-                  <a href="">Career</a>
-                </div>
-              </li>
-              <li>
-                <h3 className="mb-3 font-medium text-black">HELP</h3>
-                <div className="flex flex-col gap-2 text-black/60">
-                  <a href="">Customer Support</a>
-                  <a href="">Delivery Details</a>
-                  <a href="">Terms & Conditions</a>
-                  <a href="">Privacy Policy</a>
-                </div>
-              </li>
-              <li>
-                <h3 className="mb-3 font-medium text-black">FAQ</h3>
-                <div className="flex flex-col gap-2 text-black/60">
-                  <a href="">Account</a>
-                  <a href="">Manage Deliveries</a>
-                  <a href="">Orders</a>
-                  <a href="">Payments</a>
-                </div>
-              </li>
-              <li>
-                <h3 className="mb-3 font-medium text-black">COMPANY</h3>
-                <div className="flex flex-col gap-2 text-black/60">
-                  <a href="">Free eBooks</a>
-                  <a href="">Development Tutorial</a>
-                  <a href="">How to - Blog</a>
-                  <a href="">Youtube Playlist</a>
-                </div>
-              </li>
-            </ul>
-            <hr className="my-2 bg-black/10" />
-            <div className="mb-16 flex flex-col items-center gap-4 lg:flex-row lg:justify-between">
+          <div className="bg-anti-flash-white px-4 pt-6 text-black lg:px-24">
+            <div className="w-full justify-between lg:flex lg:gap-60">
+              <div className="flex flex-col gap-3 lg:w-1/5 lg:gap-6">
+                <h2 className={`${integral.className} text-3xl text-black`}>
+                  SHOP.CO
+                </h2>
+                <p className="text-black/60">
+                  We have clothes that suits your style and which you’re proud
+                  to wear. From women to men.
+                </p>
+                <ul className="flex gap-3 pb-4">
+                  <li className="rounded-full border border-black/20 bg-white p-2">
+                    <a href="">
+                      <Twitter size={20} />
+                    </a>
+                  </li>
+                  <li className="rounded-full border border-black/20 bg-white p-2">
+                    <a href="">
+                      <Facebook size={20} />
+                    </a>
+                  </li>
+                  <li className="rounded-full border border-black/20 bg-white p-2">
+                    <a href="">
+                      <Instagram size={20} />
+                    </a>
+                  </li>
+                  <li className="rounded-full border border-black/20 bg-white p-2">
+                    <a href="">
+                      <Github size={20} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <ul className="grid grid-cols-2 gap-y-6 pb-6 lg:flex lg:w-4/5 lg:justify-between lg:pb-12">
+                <li>
+                  <h3 className="mb-3 font-medium text-black lg:mb-5">
+                    COMPANY
+                  </h3>
+                  <div className="flex flex-col gap-2 text-black/60 lg:gap-4">
+                    <a href="">About</a>
+                    <a href="">Features</a>
+                    <a href="">Works</a>
+                    <a href="">Career</a>
+                  </div>
+                </li>
+                <li>
+                  <h3 className="mb-3 font-medium text-black lg:mb-5">HELP</h3>
+                  <div className="flex flex-col gap-2 text-black/60 lg:gap-4">
+                    <a href="">Customer Support</a>
+                    <a href="">Delivery Details</a>
+                    <a href="">Terms & Conditions</a>
+                    <a href="">Privacy Policy</a>
+                  </div>
+                </li>
+                <li>
+                  <h3 className="mb-3 font-medium text-black lg:mb-5">FAQ</h3>
+                  <div className="flex flex-col gap-2 text-black/60 lg:gap-4">
+                    <a href="">Account</a>
+                    <a href="">Manage Deliveries</a>
+                    <a href="">Orders</a>
+                    <a href="">Payments</a>
+                  </div>
+                </li>
+                <li>
+                  <h3 className="mb-3 font-medium text-black lg:mb-5">
+                    COMPANY
+                  </h3>
+                  <div className="flex flex-col gap-2 text-black/60 lg:gap-4">
+                    <a href="">Free eBooks</a>
+                    <a href="">Development Tutorial</a>
+                    <a href="">How to - Blog</a>
+                    <a href="">Youtube Playlist</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <hr className="bg-black/10" />
+            <div className="flex flex-col items-center gap-4 py-4 pb-24 lg:flex-row lg:justify-between">
               <p className="text-black/60">
                 Shop.co © 2000-2023, All Rights Reserved
               </p>
-              <div className="flex w-3/4 gap-4 lg:w-fit">
-                <Image src={visa} alt="Visa" className="w-full lg:w-auto" />
+              <div className="flex w-fit gap-4">
+                <Image
+                  src={visa}
+                  alt="Visa"
+                  className="w-[40px] rounded lg:w-[45px]"
+                />
                 <Image
                   src={mastercard}
                   alt="MasterCard"
-                  className="w-full lg:w-auto"
+                  className="w-[40px] rounded lg:w-[45px]"
                 />
-                <Image src={paypal} alt="PayPal" className="w-full lg:w-auto" />
+                <Image
+                  src={paypal}
+                  alt="PayPal"
+                  className="w-[40px] rounded lg:w-[45px]"
+                />
                 <Image
                   src={applepay}
                   alt="ApplePay"
-                  className="w-full lg:w-auto"
+                  className="w-[40px] rounded lg:w-[45px]"
                 />
                 <Image
                   src={googlepay}
                   alt="GooglePay"
-                  className="w-full lg:w-auto"
+                  className="w-[40px] rounded lg:w-[45px]"
                 />
               </div>
             </div>
